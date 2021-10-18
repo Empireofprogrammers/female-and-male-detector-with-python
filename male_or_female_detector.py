e64ed4d97fb7b9289e49a94b4e78f46a515368ab
Vishnu <92714435+Empireofprogrammers@users.noreply.github.com>
2021-10-18 15:42:42 +0530

    Update male_or_female_detector.py

--- male_or_female_detector.py
+++ male_or_female_detector.py
@@ -1,15 +1,15 @@
-name = input("What is your name? ").capitalize()
-male_names = open("male_names","r")
-Write_in_males = open("male_names","a+")
+name = input("What is your name? ").capitalize() # this will capitilize the input that we get from the user
+male_names = open("male_names","r")#there is a file calles male_name from which i am going to read data
+Write_in_males = open("male_names","a+")#No i also want to add some data in male_name also that's why created this varible
 female_names = open("female_names","r")
 write_in_females = open("female_names","a+")
 
-if name in male_names.read():
+if name in male_names.read():# making a condition that if the user name is in the file male_names
     print("You are a male")
-elif name in female_names.read():
+elif name in female_names.read(): #making a elif condition that if  name is in female_names
     print("You are a female")
 else:
-    if name.endswith("i") or name.endswith("a") or name.endswith("u"):
+    if name.endswith("i") or name.endswith("a") or name.endswith("u"): 
         print("Maybe you are a female")
         is_gender_correct = input("Is your gender correct y/n:- ")
         if is_gender_correct == "n":
@@ -27,3 +27,4 @@ else:
         else:
             print("Thanks for your feedback")
             Write_in_males.write(name + "\n")
+#Now we end up are full project        
